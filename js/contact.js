52272cafb85893915e51f0937649a3514ef93750
Rohini Lawrence <rohinilawrence@gmail.com>
2020-07-18 23:45:09 -0400

    download + exel

--- js/contact.js
+++ js/contact.js
@@ -12,71 +12,111 @@ $(document).ready(function(){
     $(function() {
         $('#contactForm').validate({
             rules: {
-                name: {
+                fname: {
                     required: true,
                     minlength: 2
                 },
-                subject: {
+                lname: {
                     required: true,
-                    minlength: 4
+                    minlength: 2
                 },
-                number: {
+                phone: {
                     required: true,
-                    minlength: 5
+                    minlength: 10,
+                    tel:true
                 },
                 email: {
                     required: true,
                     email: true
-                },
-                message: {
-                    required: true,
-                    minlength: 20
                 }
             },
             messages: {
-                name: {
+                fname: {
                     required: "come on, you have a name, don't you?",
                     minlength: "your name must consist of at least 2 characters"
                 },
-                subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
+                lname: {
+                    required: "don't forget to mention your last name!",
+                    minlength: "your last name must consist of at least 2 characters"
                 },
-                number: {
+                phone: {
                     required: "come on, you have a number, don't you?",
-                    minlength: "your Number must consist of at least 5 characters"
+                    minlength: "your Number must consist of at least 10 characters"
                 },
                 email: {
-                    required: "no email, no message"
-                },
-                message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
+                    required: "no email, no pdf!"
                 }
             },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    url:"contact_process.php",
-                    success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
+            submitHandler: function() {
+                // $(form).ajaxSubmit({
+                //     type:"POST",
+                //     data: $(form).serialize(),
+                //     url:"contact_process.php",
+                //     success: function() {
+                //         $('#contactForm :input').attr('disabled', 'disabled');
+                //         $('#contactForm').fadeTo( "slow", 1, function() {
+                //             $(this).find(':input').attr('disabled', 'disabled');
+                //             $(this).find('label').css('cursor','default');
+                //             $('#success').fadeIn()
+                //             $('.modal').modal('hide');
+		        //         	$('#success').modal('show');
+                //         })
+                //     },
+                //     error: function() {
+                //         $('#contactForm').fadeTo( "slow", 1, function() {
+                //             $('#error').fadeIn()
+                //             $('.modal').modal('hide');
+		        //         	$('#error').modal('show');
+                //         })
+                //     }
+                // })
+
+                const url = "https://script.google.com/macros/s/AKfycbyC2HdYr6qcvDvMEkccFsMSmKlXsowHsu6YuEVslP5WPmS6-2Ls/exec";
+
+                // document.getElementById("btn").addEventListener("click", insert_value);
+                // function get_value(){
+                // fetch(url)
+                //         .then(d=> d.json())
+                //         .then(d=> {
+                //             document.getElementById("app").textContent= d[0].status;
+                //             });
+                // }
+            
+                // function insert_value(){
+                var fname =document.getElementById("fname").value;
+                var lname = document.getElementById("lname").value;
+                var email = document.getElementById("email").value;
+                var phone = document.getElementById("phone").value;
+                
+                
+                var info ={
+                    "First Name" : fname,
+                    "Last Name":lname,
+                    "Phone": phone,
+                    "Email": email,
+                };
+            
+                console.log(info);
+            
+                    fetch(url, { 
+                            method: 'POST',
+                            mode: 'no-cors',
+                            cache: 'no-cache',
+                            headers: {
+                                'Content-Type' : 'application/json'
+                            },
+                            redirect: 'follow',
+                            body: JSON.stringify(info)
                         })
-                    },
-                    error: function() {
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $('#error').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#error').modal('show');
-                        })
-                    }
-                })
+                        // .then(res => res.status)
+                        // .then(d=> d.json())
+                        .then(d=> {
+                            console.log(d);
+                            });
+            
+                  
+                    
+                }
             }
         })
     })
--- js/main.js
+++ js/main.js
@@ -30,6 +30,7 @@
     const url = "https://script.google.com/macros/s/AKfycbyC2HdYr6qcvDvMEkccFsMSmKlXsowHsu6YuEVslP5WPmS6-2Ls/exec";
 
     document.getElementById("btn").addEventListener("click", insert_value);
+    document.getElementById("btn2").addEventListener("click", insert_value);
     function get_value(){
     fetch(url)
             .then(d=> d.json())
@@ -46,10 +47,10 @@
     
     
     var info ={
-        First : fname,
-        Last :lname,
-        Phone: phone,
-        Email: email,
+        "First Name" : fname,
+        "Last Name":lname,
+        "Phone": phone,
+        "Email": email,
     };
 
     console.log(info);
@@ -62,19 +63,42 @@
                     'Content-Type' : 'application/json'
                 },
                 redirect: 'follow',
-                body: JSON.stringify({First : fname,
-                    Last :lname,
-                    Phone: phone,
-                    Email: email})
+                body: JSON.stringify(info)
             })
             // .then(res => res.status)
             // .then(d=> d.json())
             .then(d=> {
-                document.getElementById("app").textContent= d[0].status;
+                console.log(d);
                 });
 
-      
-        
+
+                // success: function() {
+                    $('#contactForm :input').attr('disabled', 'disabled');
+                    $('#contactForm').fadeTo( "slow", 1, function() {
+                        $(this).find(':input').attr('disabled', 'disabled');
+                        $(this).find('label').css('cursor','default');
+                        $('#success').fadeIn()
+                        $('.modal').modal('hide');
+                        $('#success').modal('show');
+                    })
+                // }
+                // error: function() {
+                //     $('#contactForm').fadeTo( "slow", 1, function() {
+                //         $('#error').fadeIn()
+                //         $('.modal').modal('hide');
+                //         $('#error').modal('show');
+                //     })
+                // }
+
+                var link = document.createElement("a");
+                // If you don't know the name or want to use
+                // the webserver default set name = ''
+                link.setAttribute('download', name = "pdf");
+                link.href = "./PDF.pdf";
+                // document.body.appendChild(link);
+                link.click();
+                link.remove();
+                
     }
 
 
